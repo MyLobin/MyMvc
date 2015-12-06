2034e70f41ee93437c2116710b199c3b7ef086c8
Lobin <lobin.tong@hotmail.com>
2015-12-06 15:37:44 +0800

    basic soa

---
+++ TestConsole/IModelService.cs
@@ -0,0 +1,17 @@
+﻿using MyEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEF.Host
+{
+    [ServiceContract]
+    interface IModelService
+    {
+        [OperationContract]
+        List<Employee> GetEmployeeList();
+    }
+}
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,7 +1,9 @@
-﻿using MyEF.Services;
+﻿using MyEF.Host;
+using MyEF.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,9 +20,9 @@ namespace TestConsole
             //{
             //    Console.WriteLine(item.FirstName);
             //}
-            ServiceReference1.ModelServiceClient client = new ServiceReference1.ModelServiceClient();
-            var list=client.GetEmployeeList();
-
+            ///Additional information: 在 ServiceModel 客户端配置部分中，找不到名称“getEmployeeList”和协定“TestConsole.IModelService”的终结点元素。这可能是因为未找到应用程序的配置文件，或者是因为客户端元素中找不到与此名称匹配的终结点元素。
+            var proxy = new ChannelFactory<IModelService>("getEmployeeList");
+            var list = proxy.CreateChannel().GetEmployeeList();
             Console.ReadKey();
         }
     }
