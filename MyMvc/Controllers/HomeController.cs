74c8b43ecc009a5079b8d19fdf07a5d467616e54
Lobin <lobin.tong@hotmail.com>
2015-12-12 17:35:59 +0800

    i don't know

--- MyMvc/Controllers/HomeController.cs
+++ MyMvc/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using MyMvc.Proxy;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,6 +18,7 @@ namespace MyMvc.Controllers
         }
         public ActionResult Test()
         {
+            Trace.TraceError("aaa");
             return Content("test");
         }
     }
